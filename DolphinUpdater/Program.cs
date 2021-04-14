5f8801441d3c7843adb19663b756eb731da9a498
Bird <39280243+jlambert360@users.noreply.github.com>
2021-04-14 18:24:37 -0400

    Let the updater extract from a subdirectory

--- DolphinUpdater/Program.cs
+++ DolphinUpdater/Program.cs
@@ -9,8 +9,9 @@ namespace DolphinUpdater
 {
     class Program
     {
-        static string currentPath = Directory.GetCurrentDirectory();
         static string path;
+        static string tempPath;
+        static string updatedPath;
         static string zipPath;
         private static int counter;
 
@@ -20,15 +21,26 @@ namespace DolphinUpdater
                 Environment.Exit(0);
 
             string downloadLink = args[0];
-            path = args[1] + "/temp/";
-            zipPath = path + "temp.zip";
+            path = args[1];
+            tempPath = path + "/temp/";
+            zipPath = tempPath + "temp.zip";
 
             await Task.Run(() => DownloadZip(downloadLink));
-            ExtractZip(zipPath, currentPath);
+            ExtractZip(zipPath, tempPath);
 
-            Directory.Delete(path, true);
+            if (File.Exists(zipPath))
+                File.Delete(zipPath);
 
-            string dolphinPath = currentPath + "/Dolphin.exe";
+            GetDolphinPath();
+
+            if (updatedPath == null)
+                updatedPath = tempPath;
+
+            MoveUpdateFiles(updatedPath, path);
+
+            Directory.Delete(tempPath, true);
+
+            string dolphinPath = path + "/Dolphin.exe";
             if (File.Exists(dolphinPath))
                 Process.Start(dolphinPath);
             else
@@ -42,8 +54,8 @@ namespace DolphinUpdater
 
         private static async Task DownloadZip(string downloadlink)
         {
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
+            if (!Directory.Exists(tempPath))
+                Directory.CreateDirectory(tempPath);
 
             if (File.Exists(zipPath))
                 File.Delete(zipPath);
@@ -120,6 +132,59 @@ namespace DolphinUpdater
                                   + ((e.TotalBytesToReceive / 1024f) / 1024f).ToString("#0.##") + "mb"
                                   + "  (" + e.ProgressPercentage + "%)"
                     );
+            }
+        }
+
+        private static void GetDolphinPath()
+        {
+            DirectoryInfo diTop = new DirectoryInfo(tempPath);
+            foreach (var di in diTop.EnumerateDirectories("*"))
+            {
+                try
+                {
+                    foreach (var fi in di.EnumerateFiles("*", SearchOption.AllDirectories))
+                    {
+                        try
+                        {
+                            if (fi.Name.ToLower() == "dolphin.exe")
+                            {
+                                updatedPath = fi.FullName.ToLower().Replace("dolphin.exe", "");
+                            }
+                        }
+                        catch (UnauthorizedAccessException unAuthFile)
+                        {
+                            Console.WriteLine($"unAuthFile: {unAuthFile.Message}");
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException unAuthSubDir)
+                {
+                    Console.WriteLine($"unAuthSubDir: {unAuthSubDir.Message}");
+                }
+            }
+        }
+
+        private static void MoveUpdateFiles(string updateFilesPath, string destinationPath)
+        {
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            foreach (string folder in Directory.GetDirectories(updateFilesPath))
+            {
+                string dest = Path.Combine(destinationPath, Path.GetFileName(folder));
+                MoveUpdateFiles(folder, dest);
+            }
+
+            foreach (string file in Directory.GetFiles(updateFilesPath))
+            {
+                string dest = Path.Combine(destinationPath, Path.GetFileName(file));
+
+                if (File.Exists(dest))
+                    File.Delete(dest);
+
+                File.Copy(file, dest);
             }
         }
     }
